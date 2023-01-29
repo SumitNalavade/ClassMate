fb7f85499231400fdc2bacce05c9e6e82ec3c0d1
SumitNalavade <vs.nalavade2003@gmail.com>
2023-01-28 19:48:18 -0600

    Updates

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -28,7 +28,7 @@ const Navbar: React.FC = () => {
       </div>
       <div className="flex-none">
         <div>
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
+          <label tabIndex={0} className="btn btn-ghost btn-circle" htmlFor="postModal">
             <div className="indicator">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
